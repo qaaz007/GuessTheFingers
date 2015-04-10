51e54c5017bb852dc73a24981ce8a97bc42e0cc1
Adnan <aaziz@Adnans-MacBook-Pro.local>
2015-04-09 20:00:24 -0700

    completed my first guess the fingers game

--- GuessTheFingers/ViewController.swift
+++ GuessTheFingers/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guessNumber: UITextField!
+    
+    @IBOutlet weak var rightGuessLabel: UILabel!
+    
+    @IBAction func play(sender: AnyObject) {
+    
+        var randomNumber = arc4random_uniform(6)
+        println(randomNumber)
+        var usersGuessNumber = guessNumber.text.toInt()
+        if (usersGuessNumber == Int(randomNumber)) {
+            rightGuessLabel.text = "Right Guess!!!"
+            rightGuessLabel.textColor = UIColor.greenColor()
+        }
+        else {
+            rightGuessLabel.text = "Try Again..."
+            rightGuessLabel.textColor = UIColor.redColor()
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
